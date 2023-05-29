c60fd2f70a98d7d46957cc6457f63ff81aad0d91
Rafael Cavalcanti <dev@rafaelc.org>
2023-05-29 00:49:05 -0300

    Revert "Add alpha to Xresources"
    This reverts commit 454abd4263709c64eeba6e6057364d0a50bbe434.

--- config.h
+++ config.h
@@ -212,7 +212,6 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
-		{ "alpha",        FLOAT,   &alpha },
 };
 
 /*
