db6c39db39910f583596bfc194a61c43f66006e8
Rafael Cavalcanti <dev@rafaelc.org>
2023-09-06 14:58:32 -0300

    Revert "Apply font2 patch"
    This reverts commit 1b62415802b73b64dbbefb87b15b0dc24727c5f1.

--- config.h
+++ config.h
@@ -6,10 +6,6 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = {
-	"PowerlineSymbols:pixelsize=16:antialias=true:autohint=true",
-};
-
 static int borderpx = 2;
 
 /*
